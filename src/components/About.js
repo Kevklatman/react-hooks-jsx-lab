1470bdce0a9ef0598730b782f82adb08d15bf6f6
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-04 18:12:12 -0600

    lab complete

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,17 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
-}
+  return(
+  <div id="about">
+    <h2>
+      About Me
+    </h2>
+    <p>
+      What's with these homies who be dissin' my girl. Why do they gotta front?
+    </p>
+    <img src={image} alt="I made this">
+    </img>
+  </div>
+)}
 
 export default About;
