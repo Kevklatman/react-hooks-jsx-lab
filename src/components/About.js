aae44c4eb4f131dbeb1f04451fc794010c7e47d6
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-04 18:44:52 -0600

    eliza thornberry

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,7 @@ function About() {
       About Me
     </h2>
     <p>
-      What's with these homies who be dissin' my girl. Why do they gotta front?
-    </p>
+    "This is me, Eliza Thornberry, part of your average family. I got a dad, a mom, and a sister. There is Donnie; we found him. And Darwin, he found us. Oh yeah, about our house. It moves, ’cause we travel all over the world. You see, my dad hosts this nature show, and my mom shoots it. Okay, so we’re not that average. And between you and me, something amazing happened… and now I can talk to animals. It’s really cool, but totally secret. And you know what? Life’s never been the same."    </p>
     <img src={image} alt="I made this">
     </img>
   </div>
